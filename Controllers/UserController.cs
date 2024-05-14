5688e8b1c8dfb74e972b2f8e063a1b239a490c13
cowqun <eminovcowqun96@gmail.com>
2024-05-14 19:11:55 +0400

    created new project

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication24.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Cowqun";
+            ViewBag.Surname = "Eminov";
+            ViewBag.Age = 23;
+            ViewBag.Country = "AZE";
+            ViewBag.PhoneNumber = "099-777";
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+app.MapControllerRoute(
+       name:"default",
+    pattern: "{controller=user}/{action=index}/{id?}");
+    
+app.Run();
